7c042cda84c31240254db1c69ca50b69eb8b2e51
ngs1810 <48265004+ngs1810@users.noreply.github.com>
2024-02-01 02:44:37 +0800

    Update pGoM.sh with major changes.
    Managed to define variable in awk command so that order will not be important now

--- pGoM.sh
+++ pGoM.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH -J Mosaicparents.sh
+#SBATCH -J pGoM.sh
 #SBATCH -o /home/%u/MosaiC-All/Log/pGoM.slurm-%j.out
 #SBATCH -p skylake,icelake
 #SBATCH -N 1
@@ -20,20 +20,20 @@ usage()
 echo "
 #
 #README
-#This script is designed for Mosaic-All  pipeline
-#It requires family VCF in trios,  in the order of
+#This script is designed for MosaiC-All  pipeline
+#It requires family VCF in trios
 #10-father
 #11-mother
 #12-child (proband/sib)
 #
 #SCRIPT EXECUTION
 #
-#sbatch $0 -v /path/to/vcf_files/ -s /path/to/ID.list.txt -o /path/to/output/
+#sbatch $0 -v /path/to/familytrio.vcf -s /path/to/ID.list.txt -o /path/to/output/
 #
 # Options
-#-v REQUIRED /path/to/vcf_files/  (directory for family VCFs)
-#-s REQUIRED /path/to/ID.list.txt (familyID.txt; $FAMILYID*.vcf)
-#-o REQUIRED /path/to/output/     (output dir) 
+#-v 	<file>		REQUIRED: familyVCF
+#-s 	<file>      	REQUIRED: A file e.g. sampleID.list (one header row and then tab-delimited columns \$BAMdir,\$ProbandID,\$Gender,\$Mother,\$Father)
+#-o 	<directory> 	REQUIRED: output directory
 "
 }
 
@@ -49,7 +49,7 @@ while [ "$1" != "" ]; do
                                         VCFDIR=$1
                                         ;;
                 -s )                    shift
-                                        FAMILYID=$1
+                                        SAMPLELIST=$1
                                         ;;
                 -o )                    shift
                                         OUTDIR=$1
@@ -60,117 +60,109 @@ while [ "$1" != "" ]; do
         shift
 done
 
-## Check for all required arguments
-if [ -z "$FAMILYID" ]; then
-    usage
-    echo "## ERROR: You need to provide a list of familyIDs that identifies the VCFs: $FAMILYID*.vcf "
-	exit 1
-fi
-
-readarray -t sample < $FAMILYID
-
-#Defined log files
-if [ ! -d "$LOGDIR" ]; then
-    mkdir -p $LOGDIR
-	echo "## INFO: Slurm log files will be placed in this location $LOGDIR" >> $LOGDIR/$FAMILYID.pGoM.log
-fi
 
 #Check if all information are provided
 
 if [ -z "$VCFDIR" ]; then
     usage
-    echo "## ERROR: You need to provide the directory where we can find the familyVCFs" >> $LOGDIR/$FAMILYID.pGoM.log
+    echo "## ERROR: You need to provide the directory where we can find all the FamilyVCFs listed in the $SAMPLELIST" > $LOGDIR/pGoM.log
     exit 1
+fi
+
+if [ -z "$SAMPLELIST" ]; then
+    usage
+    echo "## ERROR: You need to provide a sample list" 
+    echo "#-s REQUIRED sampleID.list (one header row and then tab-delimited columns \$BAMdir,\$ProbandID,\$Gender,\$Mother,\$Father,\$FamilyVCFID)" >> $LOGDIR/pGoM.log
+	exit 1
 fi
 
 if [ ! -d "${OUTDIR}" ]; then
     mkdir -p ${OUTDIR}
-	echo "## INFO: output directory created, you'll find all of the outputs and log files in here: ${OUTDIR}" >> $LOGDIR/$FAMILYID.pGoM.log
+	echo "## INFO: output directory created, you'll find all of the outputs and log files in here: ${OUTDIR}" >> $LOGDIR/pGoM.log
 fi
 
-##Inspect VCFs
-VCF=$VCFDIR/${sample[$SLURM_ARRAY_TASK_ID]}*.vcf
+#Array from a list of Samples (ignoring the header of the file)
+mapfile -t SAMPLEID < <(tail -n +2 "$SAMPLELIST")
 
-#Check if at least 12 columns are present
-#if [ "${#vcf_lines[@]}" -lt 12 ]; then
-#    echo "The VCF file does not have enough columns." > $LOGDIR/FAMILYID.pGoM.log
-#    exit 1
-#fi
+# Iteration starts here
+for SAMPLEID in "${SAMPLEID[@]}"; do
 
-# Get the headers from the first line, and define
-#headers=(${VCF[0]})
-#FATHER="${headers[10]}"
-#MOTHER="${headers[11]}"
-#CHILD="${headers[12]}"
+    #Defining variables from each row
 
-#Comment:
-#Attempt to combine both parents, but the VCFs order will disturb.
-##################################################################   FATHER    ####################################################################################################################
-#if [[ -n "$FATHER"  && -n "$PROBAND"]]; then
+    	ProbandID=$(awk '{print $2}' <<< "$SAMPLEID ")
+   	MotherID=$(awk '{print $4}' <<< "$SAMPLEID ")
+   	FatherID=$(awk '{print $5}' <<< "$SAMPLEID ")
+    	FamilyVCF=$(awk '{print $6}' <<< "$SAMPLEID ")
+     	VCF=$VCFDIR/$FamilyVCF
 
-    		#fa.all.inherited (just to check if tallies with previous count)
-			bcftools view $VCF |grep "^#"  > $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.all.inherited.vcf
-			bcftools view $VCF |awk '$12 ~ /^0\/1/ && $10 ~ /^0\/1/ && $11 ~/^0\/0/ {OFS="\t"; print}' >> $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.all.inherited.vcf
+     	echo "pGoM Pipeline for $ProbandID, $MotherID, $FatherID based on familyvcf:$VCF" >> $OUTDIR/$ProbandID.pipeline.log
 
-			#DP20 in father
-			bcftools view -O v -f PASS -i 'FORMAT/DP[0]>=20' $VCF > $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.DP20.vcf
+     	#Check if the VCF is available in the specified directory or else dont proceed
+       	if [ -s "$VCF" ]; then
+    	echo "$VCF exists and is not empty." >> $LOGDIR/$ProbandID.pGoM.pipeline.log
+	else
+    	echo "$VCF DOES NOT exist or is empty." >> $LOGDIR/$ProbandID.pGoM.pipeline.log
+	fi
 
-			#aaf filter in father
-			bcftools view -O v -f PASS -i '(AD[0:1]/FORMAT/DP[0])<=0.3 | (AD[0:1]/FORMAT/DP[0])>=0.7' $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.DP20.vcf > $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.mosaic.DP20.vcf
-			VCF2fa=$OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.mosaic.DP20.vcf
+	# Find column numbers for sample IDs and store in variables
+	col_P=$(grep -m 1 "^#CHROM" "$VCF" | awk -v proband="$ProbandID" '{for (i=10; i<=NF; i++) {if ($i == proband) print i}}')
+	col_M=$(grep -m 1 "^#CHROM" "$VCF" | awk -v mother="$MotherID" '{for (i=10; i<=NF; i++) {if ($i == mother) print i}}')
+	col_F=$(grep -m 1 "^#CHROM" "$VCF" | awk -v father="$FatherID" '{for (i=10; i<=NF; i++) {if ($i == father) print i}}')
 
-			#GT filter
-			bcftools view $VCF2fa |grep "^#" > $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.p1.fa.mosaic.DP20.vcf
-			bcftools view $VCF2fa | awk '$10 ~ /^0\/1/ && $11 ~ /^0\/0/ && $12 ~/^0\/1/ {OFS="\t"; print}' >> $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.mosaic.DP20.vcf
+	#Filtering for father
+ 	
+  	## Filter 1: All inherited variants from the father
+ 	bcftools view "$VCF" | grep "^#" > $OUTDIR/$ProbandID.fa.all.inherited.vcf
+ 	bcftools view "$VCF" | awk -v col_P="$col_P" -v col_M="$col_M" -v col_F="$col_F" 'BEGIN {OFS="\t"} col_F ~ /^0\/1/ && col_P ~ /^0\/1/ && col_M ~ /^0\/0/ {print}' >> $OUTDIR/$ProbandID.fa.all.inherited.vcf
 
-			#make sure children is within 0.3-0.7
-			bcftools view -O v -f PASS -i '(AD[2:1]/FORMAT/DP[2])>=0.3 && (AD[2:1]/FORMAT/DP[2])<=0.7' $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.mosaic.DP20.vcf > $OUTDIR/$CHILD.$FATHER.fa.final.vcf
+	# Filter 2: DP20 of inherited variants for all sample 
+	bcftools view -O v -f PASS -i 'FORMAT/DP>=20' "$VCF" > $OUTDIR/$ProbandID.fa.DP20.vcf
+			
+   	# Filter 3: aaf filter in father
+	bcftools view -O v -f PASS -i '(AD[0]/FORMAT/DP[0])<=0.4 | (AD[0]/FORMAT/DP[0])>=0.7' --samples $FatherID $OUTDIR/$ProbandID.fa.DP20.vcf > $OUTDIR/$ProbandID.fa.mosaic.DP20.vcf
 
-			#COUNTS
+	# Filter 4: aaf filter children is within 0.4-0.7
+	bcftools view -O v -f PASS -i '(AD[0]/FORMAT/DP[0])>=0.4 && (AD[0]/FORMAT/DP[0])<=0.7' --samples "$ProbandID" $OUTDIR/$ProbandID.fa.mosaic.DP20.vcf > $OUTDIR/$ProbandID.fa.final.vcf
 
-			V=$( bcftools view $VCF | grep -v "^#" | wc -l)
-			A=$(grep -v "^#" $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.all.inherited.vcf | wc -l)
-			B=$(grep -v "^#" $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.DP20.vcf| wc -l)
-			C=$(grep -v "^#" $VCF2fa | wc -l)
-			D=$(grep -v "^#" $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.mosaic.DP20.vcf | wc -l)
-			E=$(grep -v "^#" $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.fa.final.vcf  | wc -l)
+	#COUNTS
+	V=$( bcftools view $VCF | grep -v "^#" | wc -l)
+	A=$(grep -v "^#" $OUTDIR/$ProbandID.fa.all.inherited.vcf | wc -l)
+	B=$(grep -v "^#" $OUTDIR/$ProbandID.fa.DP20.vcf| wc -l)
+	C=$(grep -v "^#" $OUTDIR/$ProbandID.fa.mosaic.DP20.vcf | wc -l)
+	D=$(grep -v "^#" $OUTDIR/$ProbandID.fa.final.vcf  | wc -l)
+					
+	echo -e "$ProbandID\tfather\tTrios\t$V\t$A\t$B\t$C\t$D" | tr " " "\t" >> $OUTDIR/mosaic.father.trios.counts.txt
+
+	#Filtering for mother
+ 	
+  	## Filter 1: All inherited variants from the mother
+ 	bcftools view "$VCF" | grep "^#" > $OUTDIR/$ProbandID.mo.all.inherited.vcf
+ 	bcftools view "$VCF" | awk -v col_P="$col_P" -v col_M="$col_M" -v col_F="$col_F" 'BEGIN {OFS="\t"} col_M ~ /^0\/1/ && col_P ~ /^0\/1/ && col_F ~ /^0\/0/ {print}' >> $OUTDIR/$ProbandID.mo.all.inherited.vcf
+
+	# Filter 2: DP20 of inherited variants for all sample 
+	bcftools view -O v -f PASS -i 'FORMAT/DP>=20' "$VCF" > $OUTDIR/$ProbandID.mo.DP20.vcf
+			
+   	# Filter 3: aaf filter in father
+	bcftools view -O v -f PASS -i '(AD[0]/FORMAT/DP[0])<=0.4 | (AD[0]/FORMAT/DP[0])>=0.7' --samples $MotherID $OUTDIR/$ProbandID.mo.DP20.vcf > $OUTDIR/$ProbandID.mo.mosaic.DP20.vcf
+
+	# Filter 4: aaf filter children is within 0.4-0.7
+	bcftools view -O v -f PASS -i '(AD[0]/FORMAT/DP[0])>=0.4 && (AD[0]/FORMAT/DP[0])<=0.7' --samples "$ProbandID" $OUTDIR/$ProbandID.mo.mosaic.DP20.vcf > $OUTDIR/$ProbandID.mo.final.vcf
+
+	#COUNTS
+	V=$( bcftools view $VCF | grep -v "^#" | wc -l)
+	A=$(grep -v "^#" $OUTDIR/$ProbandID.mo.all.inherited.vcf | wc -l)
+	B=$(grep -v "^#" $OUTDIR/$ProbandID.mo.DP20.vcf| wc -l)
+	C=$(grep -v "^#" $OUTDIR/$ProbandID.mo.mosaic.DP20.vcf | wc -l)
+	D=$(grep -v "^#" $OUTDIR/$ProbandID.mo.final.vcf  | wc -l)
+					
+	echo -e "$ProbandID\tfather\tTrios\t$V\t$A\t$B\t$C\t$D" | tr " " "\t" >> $OUTDIR/mosaic.mother.trios.counts.txt
+
+ 	echo "pGoM Pipeline for $ProbandID is completed" >> $LOGDIR/$ProbandID.pipeline.log
+
+done
+
 
 						
-			echo -e "${sample[$SLURM_ARRAY_TASK_ID]}\tfather\tTrios\t$V\t$A\t$B\t$C\t$D\t$E" | tr " " "\t" >> $OUTDIR/mosaic.father.trios.counts.txt
+	
 
-#fi
 
-############################################################## MOTHER    ####################################################################################################################
-
-#if [[ -n "$MOTHER" && -n "$PROBAND"]]; then
-
-			#mo.all.inherited (just to check if tallies with previous count)
-			bcftools view $VCF |grep "^#"  > $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.all.inherited.vcf
-			bcftools view $VCF |awk '$12 ~ /^0\/1/ && $11 ~ /^0\/1/ && $10 ~/^0\/0/ {OFS="\t"; print}' >> $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.all.inherited.vcf
-
-			#DP20 in mother
-			bcftools view -O v -f PASS -i 'FORMAT/DP[1]>=20' $VCF > $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.DP20.vcf
-
-			#aaf filter in mother
-			bcftools view -O v -f PASS -i '(AD[1:1]/FORMAT/DP[1])<=0.3 | (AD[1:1]/FORMAT/DP[1])>=0.7' $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.DP20.vcf > $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.mosaic.DP20.vcf
-			VCF2mo=$OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.mosaic.DP20.vcf
-
-			#GT filter
-			bcftools view $VCF2mo |	grep "^#" > $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.p1.mo.mosaic.DP20.vcf
-			bcftools view $VCF2mo |	awk '$10 ~ /^0\/0/ && $11 ~ /^0\/1/ && $12 ~/^0\/1/ {OFS="\t"; print}' >> $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.mosaic.DP20.vcf
-
-			#make sure children is within 0.3-0.7
-			bcftools view -O v -f PASS -i '(AD[2:1]/FORMAT/DP[2])>=0.3 && (AD[2:1]/FORMAT/DP[2])<=0.7' $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.mosaic.DP20.vcf > $OUTDIR/$CHILD.$MOTHER.mo.final.vcf
-
-			#COUNTS
-			V=$( bcftools view $VCF | grep -v "^#" | wc -l)
-			A=$(grep -v "^#" $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.all.inherited.vcf | wc -l)
-			B=$(grep -v "^#" $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.DP20.vcf| wc -l)
-			C=$(grep -v "^#" $VCF2mo | wc -l)
-			F=$(grep -v "^#" $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.mosaic.DP20.vcf | wc -l)
-			G=$(grep -v "^#" $OUTDIR/${sample[$SLURM_ARRAY_TASK_ID]}.mo.final.vcf  | wc -l)
-
-						
-			echo -e "${sample[$SLURM_ARRAY_TASK_ID]}\tmother\tTrios\t$V\t$A\t$B\t$C\t$D\t$E" | tr " " "\t" >> $OUTDIR/mosaic.mother.trios.counts.txt
-
-#fi
